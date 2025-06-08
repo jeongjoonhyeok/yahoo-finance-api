056cc3616444601af269d13e7b228f4dbc31726a
jeongjoonhyeok <jeongjoonhyeok@gmail.com>
2025-06-08 19:02:45 +0900

    history: return error on JSON decode failure in GetHistory

--- history.go
+++ history.go
@@ -150,6 +150,7 @@ func (h *History) GetHistory(symbol string) (YahooHistoryRespose, error) {
 	var historyResponse YahooHistoryRespose
 	if err := json.NewDecoder(resp.Body).Decode(&historyResponse); err != nil {
 		log.Fatalf("Failed to decode history data JSON response: %v", err)
+		return YahooHistoryRespose{}, err
 	}
 
 	if len(historyResponse.Chart.Result) == 0 {
